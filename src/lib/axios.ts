718b82072d5758accde9ca451237eba6ecc4b6c8
WilliamD3v <williamD3v@gmail.com>
2025-04-14 23:24:47 -0300

    Pagina de agendamento em processo

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 /* https://barberly-back.vercel.app */
 /* http://localhost:5000/ */
-const URL = "http://localhost:5000/";
+const URL = "https://barberly-back.vercel.app/";
 
 export default axios.create({
   baseURL: URL,
